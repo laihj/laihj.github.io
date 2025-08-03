16b42c366fb08d86c52fd32f775082bd740854f0
laihj <hamainter@gmail.com>
2025-08-03 14:06:33 +0800

    update: 2025-08-03 14:06:33

--- scripts/new-daily.js
+++ scripts/new-daily.js
@@ -1,5 +1,6 @@
 hexo.extend.console.register('daily', 'Create a new daily post.', {}, function(args) {
   const moment = require('moment');
+  const { spawn } = require('child_process');
   
   // Generate today's date in yyyyMMdd format
   const today = moment().format('YYYYMMDD');
@@ -7,5 +8,17 @@ hexo.extend.console.register('daily', 'Create a new daily post.', {}, function(a
   // Create the post using the daily scaffold
   return this.call('new', {
     _: ['daily', today]
+  }).then(() => {
+    // Auto open the created file in VSCode
+    const fileName = `${moment().format('YYYY-MM-DD')}-${today}.md`;
+    const filePath = `source/_posts/${fileName}`;
+    
+    // Open in VSCode
+    spawn('code', [filePath], { 
+      stdio: 'ignore',
+      detached: true 
+    });
+    
+    this.log.info(`Opening ${filePath} in VSCode...`);
   });
 });
